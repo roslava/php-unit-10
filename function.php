3b60ca782fa8594818af6303b2d2db19a9b2e79b
roslava <roslav@icloud.com>
2021-11-12 11:22:47 +0300

    second commit

--- function.php
+++ function.php
@@ -55,15 +55,52 @@ function t7($path, $str)
 
 function t8($path)
 {
+// ПОЛУЧЕНИЕ СОДЕРЖИМОГО ПАПКИ
 
+    if ($handle = opendir($path)) {
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry != "." && $entry != "..") {
+                echo "$entry"." — ";
+                echo filesize($path.$entry).'<br>';
+//                var_dump(filesize($path.$entry).'<br>');
+            }
+        }
+        closedir($handle);
+    }
 }
 
 function t9($path)
 {
+// ПОЛУЧЕНИЕ СОДЕРЖИМОГО ПАПКИ
+    $allArr = [];
+    if ($handle = opendir($path)) {
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry != "." && $entry != "..") {
 
+// Соединяем в одну строку путь, базовое имя файла, и размер файла.
+// Далее разбиваем на элементы массива функцией explode() с сепаратором ".".
+                $nested_array = explode(".", $path . $entry . '.' . filesize($path . $entry));
+// Убираем пустой элемент из начала массива (то, что было до точки в строке пути к файлу)
+                array_shift($nested_array);
+// К значению первого элемента массива добавляем точку — в начало.
+                $nested_array[0] = "." . $nested_array[0];
+// Добавляем получившийся массив в качестве вложенного в главный массив $allArr.
+                $allArr [] = $nested_array;
+            }
+        }
+        closedir($handle);
+        return $allArr;
+    }
 }
 
 function t10($path)
 {
-
+if(file_exists($path)){
+    return 0;
+}else{
+    $fp = fopen($path, 'w');
+    fwrite($fp, 42);
+    fclose($fp);
+    return 1;
+}
 }
